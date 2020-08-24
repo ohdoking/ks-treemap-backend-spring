47069f5425196e8d46bedf8ff168dea9f615840b
Dokeun Oh <dokeun.oh@car-ewallet.de>
2020-08-24 17:57:37 +0200

    init

---
+++ src/main/java/com/ohdoking/kstreemap/KstreemapApplication.java
@@ -0,0 +1,13 @@
+package com.ohdoking.kstreemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstreemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstreemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohdoking/kstreemap/KstreemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohdoking.kstreemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstreemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
